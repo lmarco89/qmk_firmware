8396dd556e6b5d602145ebc6cb1d21c4201ab619
luke marco <luke.am89@hotmail.co.uk>
2020-05-31 17:13:57 +0100

    Added luke vim dz60 and crkbd gamepad builds

--- keyboards/crkbd/lib/layer_state_reader.c
+++ keyboards/crkbd/lib/layer_state_reader.c
@@ -9,6 +9,8 @@
 #define L_RAISE 4
 #define L_ADJUST 8
 #define L_ADJUST_TRI 14
+#define _NUMPAD 16
+#define _NAV 32
 
 char layer_state_str[24];
 
@@ -16,7 +18,7 @@ const char *read_layer_state(void) {
   switch (layer_state)
   {
   case L_BASE:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Default");
+    snprintf(layer_state_str, sizeof(layer_state_str), "Mode: Gamepad");
     break;
   case L_RAISE:
     snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Raise");
@@ -27,6 +29,12 @@ const char *read_layer_state(void) {
   case L_ADJUST:
   case L_ADJUST_TRI:
     snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Adjust");
+    break;
+  case _NUMPAD:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Mode: Numpad");
+    break;
+  case _NAV:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Mode: Nav");
     break;
   default:
     snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Undef-%ld", layer_state);
